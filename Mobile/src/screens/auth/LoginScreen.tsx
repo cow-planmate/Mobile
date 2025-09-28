3b6babc7b2f94888894d1d3fab91a373589ea629
민영 <min301313@gmail.com>
2025-09-28 17:39:35 +0900

    React Navigation 라이브러리를 설치

---
+++ Mobile/src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,19 @@
+// src/screens/auth/LoginScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>로그인 화면</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ Mobile/src/screens/auth/SignupScreen.tsx
@@ -0,0 +1,19 @@
+// src/screens/auth/SignupScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SignupScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>회원가입 화면</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
