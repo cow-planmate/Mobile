bf66d02d757a94da5f6dd6c8a5d7a203457e04c9
민영 <min301313@gmail.com>
2026-08-07 15:36:18 +0900

    test: 테스트 파일에 맞는 린트 규칙을 적용합니다

--- Mobile/.eslintrc.js
+++ Mobile/.eslintrc.js
@@ -6,6 +6,17 @@ module.exports = {
   },
   overrides: [
     {
+      files: ['__tests__/**/*.{js,jsx,ts,tsx}', 'jest.setup.js'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        '@typescript-eslint/no-shadow': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+      },
+    },
+    {
       // Storybook의 `render`는 이름만 소문자일 뿐 실제로는 컴포넌트 렌더 함수라
       // 훅 사용이 정상이다. 규칙이 이를 구분하지 못해 오탐이 발생한다.
       files: ['*.stories.tsx', '*.stories.ts'],
